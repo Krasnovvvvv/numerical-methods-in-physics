1fa13d950253c8ef28eef8358ee21ecf8c6d2343
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-09-21 19:17:38 +0300

    feat: added IDataGenerator class for generating input data

---
+++ IDataGenerator.h
@@ -0,0 +1,14 @@
+
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_IDATAGENERATOR_H
+#define NUMERICAL_METHODS_IN_PHYSICS_IDATAGENERATOR_H
+
+#pragma once
+#include <Eigen/Dense>
+class IDataGenerator {
+public:
+    virtual Eigen::MatrixXd generateMatrix(size_t n) = 0;
+    virtual Eigen::VectorXd generateVector(size_t n) = 0;
+    virtual ~IDataGenerator() = default;
+};
+
+#endif //NUMERICAL_METHODS_IN_PHYSICS_IDATAGENERATOR_H
