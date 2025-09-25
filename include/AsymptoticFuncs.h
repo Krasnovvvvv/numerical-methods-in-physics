350a0813f7e7db1d4da26c05992cda2b5f1f7508
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-09-25 15:17:46 +0300

    fix: fixed errors in linearAsymptotic function

--- include/AsymptoticFuncs.h
+++ include/AsymptoticFuncs.h
@@ -5,17 +5,26 @@
 #include <vector>
 
 inline std::vector<double> linearAsymptotic(const std::vector<double>& x, const std::vector<double>& y) {
-    double sum_x2 = 0, sum_xy = 0;
-    for (size_t i = 0; i < x.size(); ++i) {
+    size_t n = x.size();
+    double sum_x = 0, sum_y = 0, sum_x2 = 0, sum_xy = 0;
+    for (size_t i = 0; i < n; ++i) {
+        sum_x += x[i];
+        sum_y += y[i];
         sum_x2 += x[i] * x[i];
         sum_xy += x[i] * y[i];
     }
-    double alpha = sum_x2 != 0 ? sum_xy / sum_x2 : 0.0;
-    std::vector<double> y_expected(x.size());
-    for (size_t i = 0; i < x.size(); ++i) {
-        y_expected[i] = alpha * x[i];
+    double mean_x = sum_x / n;
+    double mean_y = sum_y / n;
+    double mean_x2 = sum_x2 / n;
+    double mean_xy = sum_xy / n;
+    double k = (mean_xy - mean_x * mean_y) / (mean_x2 - mean_x * mean_x);
+    double b = mean_y - k * mean_x;
+    std::vector<double> y_expected(n);
+    for (size_t i = 0; i < n; ++i) {
+        y_expected[i] = k * x[i] + b;
     }
     return y_expected;
 }
+
 
 #endif //NUMERICAL_METHODS_IN_PHYSICS_ASYMPTOTICFUNCS_H
