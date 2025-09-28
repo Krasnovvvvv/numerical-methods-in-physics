e86752337d81a98d4b55c85af702b495075e109f
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-09-28 14:53:44 +0300

    feat: added task class for iter methods

--- include/ISolver.h
+++ include/ISolver.h
@@ -3,16 +3,26 @@
 
 #pragma once
 #include <Eigen/Dense>
+#include <optional>
+
 struct SolveResult {
     Eigen::VectorXd solution;
     size_t iterations;
     double residual;
+    std::vector<std::pair<size_t, double>> residuals;
 };
 
 class ISolver {
 public:
     virtual SolveResult solve(const Eigen::MatrixXd&, const Eigen::VectorXd&) = 0;
     virtual ~ISolver() = default;
+
+    explicit ISolver(std::optional<int> max_iter = std::nullopt,
+            std::optional<double> tol = std::nullopt) : maxIter(max_iter), tolerance(tol) {}
+
+protected:
+    std::optional<int> maxIter;
+    std::optional<double> tolerance;
 };
 
 #endif //NUMERICAL_METHODS_IN_PHYSICS_ISOLVER_H
---
+++ include/SLAE_IterMethod_Task.h
@@ -0,0 +1,30 @@
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_SLAE_ITERMETHOD_TASK_H
+#define NUMERICAL_METHODS_IN_PHYSICS_SLAE_ITERMETHOD_TASK_H
+
+#include "LabTask.h"
+#include "RandomSLAEGenerator.h"
+#include <vector>
+
+class SLAE_IterMethod_Task : public LabTask {
+public:
+    using LabTask::LabTask;
+
+    void run(const std::vector<size_t>& sizes) override {
+        std::vector<double> xpoints, ypoints;
+        auto* slaeGen = dynamic_cast<RandomSLAEGenerator*>(&generator);
+        if (!slaeGen) throw std::runtime_error("Generator must be RandomSLAEGenerator!");
+        size_t n = sizes.front();
+        Eigen::MatrixXd A = slaeGen->generateMatrix(n, true);
+        Eigen::VectorXd x_exact = slaeGen->exactSolution(n);
+        Eigen::VectorXd b = A * x_exact;
+
+        SolveResult result = solver.solve(A, b);
+        for (const auto& [iter, relres] : result.residuals) {
+            xpoints.push_back(static_cast<double>(iter));
+            ypoints.push_back(relres);
+        }
+        plotter.plot(xpoints, ypoints, "Relative residual", "Iteration", "||Ax-b||/||b||", true);
+    }
+};
+
+#endif //NUMERICAL_METHODS_IN_PHYSICS_SLAE_ITERMETHOD_TASK_H
