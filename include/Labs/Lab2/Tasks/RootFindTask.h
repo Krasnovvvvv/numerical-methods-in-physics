e334a929a3d8bf956e7b1165861daf2c7ecc139d
Krasnovvvvv <fedorokivan@gmail.com>
2025-10-05 23:38:43 +0300

    feat: added bisection method

--- include/Labs/Lab2/Tasks/RootFindTask.h
+++ include/Labs/Lab2/Tasks/RootFindTask.h
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <functional>
 #include <chrono>
+#include <iomanip>
 
 class RootFindTask {
 public:
@@ -17,7 +18,7 @@ public:
         std::function<double(double)> func,
         std::function<bool(double)> isInDomain,
         double a, double b,
-        double step, double tol
+        double step, double tol, size_t max_iter = 100
     ) {
         Timer<std::chrono::microseconds> timer;
         auto result = finder.findSignChange(func, isInDomain, a, b, step, tol);
@@ -29,15 +30,47 @@ public:
             std::cout << "f(left) = " << func(result->left)
                       << ", f(right) = " << func(result->right) << std::endl;
             std::cout << "Elapsed: " << (elapsed_us)
-                      << " microseconds (" << elapsed_us << " us)\n";
+                      << " microseconds\n";
+            // Уточняем корень методом дихотомии на найденном интервале:
+            double root = refineRootBisection(func, result->left, result->right, tol);
+            std::cout << "Refined root: x = " <<std::setprecision(15)<< root
+                      << ", f(x) = " << func(root) << std::endl;
         } else {
             std::cout << "No sign change found in interval ["
                       << a << ", " << b << "]\n";
         }
+
     }
 
 private:
     IRootFinder& finder;
+
+    double refineRootBisection(
+    std::function<double(double)> f,
+    double a, double b,
+    double tol = 1e-12,
+    size_t max_iter = 100
+) {
+        double fa = f(a), fb = f(b);
+        if (fa * fb > 0)
+            throw std::runtime_error("No sign change on interval for bisection!");
+
+        for (size_t iter = 0; iter < max_iter && (b - a) > tol; ++iter) {
+            double c = 0.5 * (a + b);
+            double fc = f(c);
+            if (std::abs(fc) < tol)
+                return c;
+            if (fa * fc < 0) {
+                b = c;
+                fb = fc;
+            } else {
+                a = c;
+                fa = fc;
+            }
+        }
+        return 0.5 * (a + b);
+    }
+
 };
 
 #endif //NUMERICAL_METHODS_IN_PHYSICS_ROOTFINDTASK_H
--- src/Labs/Lab2/Task1.cpp
+++ src/Labs/Lab2/Task1.cpp
@@ -1,8 +1,6 @@
-#include "Labs/Lab2/RootFinders/BruteForceRootFinder.h"
+#include "Labs/Lab2/RootFinders/ParallelRootFinder.h"
 #include "Labs/Lab2/Tasks/RootFindTask.h"
-
 #include <cmath>
-#include <iostream>
 
 int main() {
 
@@ -12,10 +10,10 @@ int main() {
     auto isInDomain = [](double x) { return x > 0; };
 
     double a = 0.1, b = 5;
-    double step = 0.001;
-    double tolerance = 1e-8;
+    double step = 1e-4;
+    double tolerance = 1e-20;
 
-    BruteForceRootFinder finder;
+    ParallelRootFinder finder;
 
     RootFindTask task(finder);
 
