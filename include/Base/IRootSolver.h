440d4658bf8717ce87da86e9596f2289daedb5a7
Krasnovvvvv <fedorokivan@gmail.com>
2025-10-05 23:53:51 +0300

    feat: added root solver interface

---
+++ include/Base/IRootSolver.h
@@ -0,0 +1,25 @@
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_IROOTSOLVER_H
+#define NUMERICAL_METHODS_IN_PHYSICS_IROOTSOLVER_H
+
+#include <vector>
+#include <functional>
+
+struct RootSolveResult {
+    double root;
+    size_t iterations;
+    double residual;
+    std::vector<std::pair<size_t, double>> residuals;
+};
+
+class IRootSolver {
+public:
+    virtual ~IRootSolver() = default;
+    virtual RootSolveResult solve(
+        std::function<double(double)> func,
+        std::function<bool(double)> isInDomain,
+        double x0, double tol,
+        size_t max_iter = 100
+    ) = 0;
+};
+
+#endif //NUMERICAL_METHODS_IN_PHYSICS_IROOTSOLVER_H
