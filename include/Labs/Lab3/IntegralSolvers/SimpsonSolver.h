cd4716b26f05f1a3a2052375dec63469b3bafc6a
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-10-10 16:46:52 +0300

    feat: added simpson solver

---
+++ include/Labs/Lab3/IntegralSolvers/SimpsonSolver.h
@@ -0,0 +1,35 @@
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_SIMPSONSOLVER_H
+#define NUMERICAL_METHODS_IN_PHYSICS_SIMPSONSOLVER_H
+
+#pragma once
+#include <Base/IIntegralSolver.h>
+
+class SimpsonSolver : public IIntegralSover {
+    std::optional<IntegrateResult> integrate(
+            std::function<double(double)> func,
+            double a, double b,
+            double tol,
+            size_t max_intervals = 1000) override {
+        size_t n = 2;
+        double prev, curr, h, error = 0;
+        std::vector<std::pair<size_t, double>> estim;
+        for (; n <= max_intervals; n *= 2) {
+            h = (b - a) / n;
+            curr = func(a) + func(b);
+            for (size_t i = 1; i < n; i += 2)
+                curr += 4.0 * func(a + h * i);
+            for (size_t i = 2; i < n; i += 2)
+                curr += 2.0 * func(a + h * i);
+            curr *= h / 3.0;
+            estim.emplace_back(n, curr);
+            if (n > 2) {
+                error = fabs(curr - prev) / 15.0;
+                if (error < tol) break;
+            }
+            prev = curr;
+        }
+        return IntegrateResult{curr, n, error, estim};
+    }
+};
+
+#endif //NUMERICAL_METHODS_IN_PHYSICS_SIMPSONSOLVER_H
