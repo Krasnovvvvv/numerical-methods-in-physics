7aae69137518dbb60406fbda26176f06b503cfa5
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-10-10 16:30:42 +0300

    feat: added central rectangles solver

---
+++ include/Labs/Lab3/IntegralSolvers/CentralRectSolver.h
@@ -0,0 +1,38 @@
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_CENTRALRECTSOLVER_H
+#define NUMERICAL_METHODS_IN_PHYSICS_CENTRALRECTSOLVER_H
+
+#pragma once
+#include <Base/IIntegralSolver.h>
+
+class CentralRectSolver : public IIntegralSover {
+public:
+    std::optional<IntegrateResult> integrate(
+            std::function<double(double)> func,
+            double a, double b,
+            double tol,
+            size_t max_intervals = 1000) override {
+        size_t n = 2;
+        double prev, curr, h, error = 0;
+        std::vector<std::pair<size_t, double>> estim;
+        for(; n <= max_intervals; n*=2) {
+            h = (b-a)/n;
+            curr = 0;
+            for (size_t i = 0; i < n; ++i) {
+                double xm = a + h * (i+ 0.5);
+                curr += func(xm);
+            }
+            curr *= h;
+            estim.emplace_back(n, curr);
+            if(n > 2) {
+                error = fabs(curr - prev) / 3.0;
+                if (error < tol) break;
+            }
+            prev = curr;
+        }
+        return struct IntegrateResult{curr, n, error, estim};
+    }
+};
+
+
+
+#endif //NUMERICAL_METHODS_IN_PHYSICS_CENTRALRECTSOLVER_H
