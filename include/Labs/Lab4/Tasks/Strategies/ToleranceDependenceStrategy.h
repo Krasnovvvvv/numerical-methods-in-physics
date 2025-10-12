e4b8fdf9c60a441b4e831657171e19fb045f70db
Krasnovvvvv <fedorokivan@gmail.com>
2025-10-12 15:09:15 +0300

    feat: added dependence on tolerance mode strategy

--- include/Labs/Lab4/Tasks/Strategies/ToleranceDependenceStrategy.h
+++ include/Labs/Lab4/Tasks/Strategies/ToleranceDependenceStrategy.h
@@ -1,32 +1,32 @@
-#ifndef NUMERICAL_METHODS_IN_PHYSICS_STEPDEPENDENCESTRATEGY_H
-#define NUMERICAL_METHODS_IN_PHYSICS_STEPDEPENDENCESTRATEGY_H
+#ifndef NUMERICAL_METHODS_IN_PHYSICS_TOLERANCEDEPENDENCESTRATEGY_H
+#define NUMERICAL_METHODS_IN_PHYSICS_TOLERANCEDEPENDENCESTRATEGY_H
 
 #pragma once
 #include "Base/IODESolver.h"
 #include "Labs/Lab4/Tasks/IODETaskStrategy.h"
 
-class StepDependenceStrategy : public IODETaskStrategy {
+class ToleranceDependenceStrategy : public IODETaskStrategy {
 public:
-    StepDependenceStrategy(IODESolver* solver) : solver_(solver) {}
+    ToleranceDependenceStrategy(IODESolver* solver) : solver_(solver) {}
 
     void run(const std::function<std::vector<double>(double, const std::vector<double>&)>& rhs,
              const std::vector<double>& y0,
              double t0, double tn,
-             double /*h*/, double tol,
+             double h, double /*tol*/,
              unsigned short graphNumber,
              Plotter* plotter,
-             std::vector<double> steps,
-             std::vector<double> /*tolerances*/,
+             std::vector<double> /*steps*/,
+             std::vector<double> tolerances,
              std::vector<IODESolver*> /*solvers*/) override
     {
         if (!solver_ || !plotter) throw std::runtime_error("Solver or Plotter not set!");
-        if (steps.empty()) throw std::invalid_argument("Steps vector is empty!");
+        if (tolerances.empty()) throw std::invalid_argument("Tolerances vector is empty!");
 
         std::vector<std::vector<double>> xs, ys;
         std::vector<std::string> labels;
 
-        for (double step : steps) {
-            auto res = solver_->solve(rhs, y0, t0, tn, step, tol);
+        for (double tol : tolerances) {
+            auto res = solver_->solve(rhs, y0, t0, tn, h, tol);
 
             std::vector<double> x, v;
             for (const auto& state : res.y) {
@@ -41,7 +41,7 @@ public:
                 case 4: xs.push_back(x); ys.push_back(v); break;
                 default: break;
             }
-            labels.push_back("h = " + std::to_string(step));
+            labels.push_back("tol = " + std::to_string(tol));
         }
         plotter->plot(xs, ys, labels,
             (graphNumber == 4 ? "x" : "t"),
@@ -52,4 +52,4 @@ private:
     IODESolver* solver_;
 };
 
-#endif //NUMERICAL_METHODS_IN_PHYSICS_STEPDEPENDENCESTRATEGY_H
+#endif //NUMERICAL_METHODS_IN_PHYSICS_TOLERANCEDEPENDENCESTRATEGY_H
